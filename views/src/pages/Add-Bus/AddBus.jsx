d80235cb18f8b9ca51dc873f32455c0f7ffd2d46
djaafarhamri <hamridjaafar12@gmail.com>
2022-07-01 12:39:01 +0100

    setup material ui

--- views/src/pages/Add-Bus/AddBus.jsx
+++ views/src/pages/Add-Bus/AddBus.jsx
@@ -1,4 +1,12 @@
 import { useState } from "react";
+import axios from "axios";
+import {
+  Typography,
+  Button,
+  TextField,
+  Grid,
+  Container,
+} from "@material-ui/core";
 
 const AddBus = () => {
   const [ref, setRef] = useState("");
@@ -11,176 +19,116 @@ const AddBus = () => {
   const [maxPersonne, setMaxPersonne] = useState(0);
   const [maxColis, setMaxColis] = useState(0);
 
-  return (
-    <div>
-      <h1>Add Bus</h1>
+  const add_bus = async () => {
+    const data = {
+      ref,
+      ticketPrice,
+      colisPrice,
+      departville,
+      arriveville,
+      departTime,
+      arriveTime,
+      maxPersonne,
+      maxColis,
+    };
+    await axios
+      .post("http://localhost:5000/bus/add_bus", data)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-      <div className="relative mb-2 mt-5">
-        <input
-          id="ref"
-          name="ref"
-          onChange={(e) => {
-            setRef(e.target.value);
-          }}
-          type="text"
-          className="peer h-10 w-96 border-b-2 border-gray-300 text-dark-blue placeholder-transparent focus:outline-none focus:border-green-cyan"
-          placeholder="‏‏‎ ‎"
+  return (
+    <div className="m-8">
+      <Typography variant="h3" component="h3">
+        Ajouter un bus
+      </Typography>
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+      >
+        <TextField
+          margin="normal"
+          id="outlined-basic"
+          label="Reference*"
+          variant="outlined"
+          size="small"
+          fullWidth
+          onChange={(e) => setRef(e.target.value)}
         />
-        <label
-          for="ref"
-          className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-6 peer-focus:text-dark-blue peer-focus:text-base"
-        >
-          reference
-        </label>
-      </div>
-      <div className="relative mb-2 mt-5">
-        <input
-          id="ticket price"
-          name="ticket price"
-          onChange={(e) => {
-            setTicketPrice(e.target.value);
-          }}
-          type="text"
-          className="peer h-10 w-96 border-b-2 border-gray-300 text-dark-blue placeholder-transparent focus:outline-none focus:border-green-cyan"
-          placeholder="‏‏‎ ‎"
+        <TextField
+          fullWidth
+          margin="normal"
+          id="outlined-basic"
+          label="Ticket price*"
+          variant="outlined"
+          size="small"
+          onChange={(e) => setTicketPrice(e.target.value)}
         />
-        <label
-          for="ticket price"
-          className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-6 peer-focus:text-dark-blue peer-focus:text-base"
-        >
-          ticket price
-        </label>
-      </div>
-      <div className="relative mb-2 mt-5">
-        <input
-          id="Colis price"
-          name="Colis price"
-          onChange={(e) => {
-            setColisPrice(e.target.value);
-          }}
-          type="text"
-          className="peer h-10 w-96 border-b-2 border-gray-300 text-dark-blue placeholder-transparent focus:outline-none focus:border-green-cyan"
-          placeholder="‏‏‎ ‎"
+        <TextField
+          fullWidth
+          margin="normal"
+          id="outlined-basic"
+          label="Colis price*"
+          variant="outlined"
+          size="small"
+          onChange={(e) => setColisPrice(e.target.value)}
         />
-        <label
-          for="Colis price"
-          className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-6 peer-focus:text-dark-blue peer-focus:text-base"
-        >
-          Colis price
-        </label>
-      </div>
-      <div className="flex">
-        <div className="relative mb-2 mt-5">
-          <input
-            id="depart ville"
-            name="depart ville"
-            onChange={(e) => {
-              setDepartVille(e.target.value);
-            }}
-            type="text"
-            className="peer h-10 w-96 border-b-2 border-gray-300 text-dark-blue placeholder-transparent focus:outline-none focus:border-green-cyan"
-            placeholder="‏‏‎ ‎"
+        <div>
+          <TextField
+            margin="normal"
+            id="outlined-basic"
+            label="Depart ville*"
+            variant="outlined"
+            size="small"
+            onChange={(e) => setDepartVille(e.target.value)}
           />
-          <label
-            for="depart ville"
-            className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-6 peer-focus:text-dark-blue peer-focus:text-base"
-          >
-            depart ville
-          </label>
-        </div>
-        <div className="relative mb-2 mt-5">
-          <input
-            id="arrive ville"
-            name="arrive ville"
-            onChange={(e) => {
-              setArriveVille(e.target.value);
-            }}
-            type="text"
-            className="peer h-10 w-96 border-b-2 border-gray-300 text-dark-blue placeholder-transparent focus:outline-none focus:border-green-cyan"
-            placeholder="‏‏‎ ‎"
+          <TextField
+            margin="normal"
+            id="outlined-basic"
+            label="Arrive ville*"
+            variant="outlined"
+            size="small"
+            onChange={(e) => setArriveVille(e.target.value)}
           />
-          <label
-            for="arrive ville"
-            className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-6 peer-focus:text-dark-blue peer-focus:text-base"
-          >
-            arrive ville
-          </label>
         </div>
-      </div>
-      <div className="flex">
-        <div className="relative mb-2 mt-5">
-          <input
-            id="depart time"
-            name="depart time"
-            onChange={(e) => {
-              setDepartTime(e.target.value);
-            }}
-            type="text"
-            className="peer h-10 w-96 border-b-2 border-gray-300 text-dark-blue placeholder-transparent focus:outline-none focus:border-green-cyan"
-            placeholder="‏‏‎ ‎"
-          />
-          <label
-            for="depart time"
-            className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-6 peer-focus:text-dark-blue peer-focus:text-base"
-          >
-            depart time
-          </label>
-        </div>
-        <div className="relative mb-2 mt-5">
-          <input
-            id="arrive time"
-            name="arrive time"
-            onChange={(e) => {
-              setArriveTime(e.target.value);
-            }}
-            type="text"
-            className="peer h-10 w-96 border-b-2 border-gray-300 text-dark-blue placeholder-transparent focus:outline-none focus:border-green-cyan"
-            placeholder="‏‏‎ ‎"
-          />
-          <label
-            for="arrive time"
-            className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-6 peer-focus:text-dark-blue peer-focus:text-base"
-          >
-            arrive time
-          </label>
-        </div>
-      </div>
-      <div className="relative mb-2 mt-5">
-        <input
-          id="max personne"
-          name="max personne"
-          onChange={(e) => {
-            setMaxPersonne(e.target.value);
-          }}
-          type="text"
-          className="peer h-10 w-96 border-b-2 border-gray-300 text-dark-blue placeholder-transparent focus:outline-none focus:border-green-cyan"
-          placeholder="‏‏‎ ‎"
+        <TextField
+          margin="normal"
+          id="outlined-basic"
+          label="Depart time*"
+          variant="outlined"
+          size="small"
+          fullWidth
+          onChange={(e) => setDepartTime(e.target.value)}
         />
-        <label
-          for="max personne"
-          className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-6 peer-focus:text-dark-blue peer-focus:text-base"
-        >
-          max personne
-        </label>
-      </div>
-      <div className="relative mb-2 mt-5">
-        <input
-          id="max colis"
-          name="max colis"
-          onChange={(e) => {
-            setMaxColis(e.target.value);
-          }}
-          type="text"
-          className="peer h-10 w-96 border-b-2 border-gray-300 text-dark-blue placeholder-transparent focus:outline-none focus:border-green-cyan"
-          placeholder="‏‏‎ ‎"
+        <TextField
+          margin="normal"
+          id="outlined-basic"
+          label="Max personne*"
+          variant="outlined"
+          size="small"
+          fullWidth
+          onChange={(e) => setMaxPersonne(e.target.value)}
         />
-        <label
-          for="max colis"
-          className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-6 peer-focus:text-dark-blue peer-focus:text-base"
-        >
-          max colis
-        </label>
-      </div>
+        <TextField
+          margin="normal"
+          id="outlined-basic"
+          label="Max colis*"
+          variant="outlined"
+          size="small"
+          fullWidth
+          onChange={(e) => setMaxColis(e.target.value)}
+        />
+        <Button variant="contained" color="primary" onClick={add_bus}>
+          Ajouter
+        </Button>
+      </Grid>
     </div>
   );
 };
